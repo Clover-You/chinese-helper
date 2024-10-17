e67312b402baa67607d336038fb291ae8911b94f
Clover You <2621869236@qq.com>
2024-10-17 11:02:14 +0800

    chore: fmt

--- src/models.rs
+++ src/models.rs
@@ -1,25 +1,25 @@
 #[derive(serde::Serialize)]
 pub struct SearchParams {
-    pub tbname: String,
-    pub show: String,
-    pub tempid: i8,
-    pub keyboard: String,
+  pub tbname: String,
+  pub show: String,
+  pub tempid: i8,
+  pub keyboard: String,
 }
 
 #[derive(Default, Debug)]
 pub struct ProverbsData {
-    pub explanation: String,
-    pub source: String,
-    pub eg: String,
-    pub pinyin: String,
-    pub zhuyin: String,
-    pub traditional: String,
-    pub feelings: String,
-    pub usage: String,
-    pub shape_disc: String,
-    pub riddle: String,
-    pub synonyms: String,
-    pub antonym: String,
-    pub english: String,
-    pub russian: String,
+  pub explanation: String,
+  pub source: String,
+  pub eg: String,
+  pub pinyin: String,
+  pub zhuyin: String,
+  pub traditional: String,
+  pub feelings: String,
+  pub usage: String,
+  pub shape_disc: String,
+  pub riddle: String,
+  pub synonyms: String,
+  pub antonym: String,
+  pub english: String,
+  pub russian: String,
 }
